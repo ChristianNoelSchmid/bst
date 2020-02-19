f360d7df3ba44c15ee04d6dad1e94e39cb443205
Christian Schmid <cschmid92@gmail.com>
2020-02-18 22:42:22 -0800

    Commenting, and attempting pointer vs. non-pointer BST implementation

--- bst.h
+++ bst.h
@@ -25,34 +25,42 @@ namespace bst
     // Non-pointer type
     template<class T> struct BSTNode
     {
-        T element;
-        BSTNode<T> *left;
-        BSTNode<T> *right;
+        T element; // The value of type T being stored in the node
+        BSTNode<T> *left; // The left child
+        BSTNode<T> *right; // The right child
 
-        BSTNode();
-        BSTNode(const T & elem);
+        BSTNode(); // Default Constructor
+        BSTNode(const T & elem); // Primary Constructor - sets element to supplied value 
 
-        BSTNode(const BSTNode<T> & source);
-        ~BSTNode();
+        BSTNode(const BSTNode<T> & source); // Copy Constructor -- deep copy of all elements
+        ~BSTNode(); // Destructor
     };
 
     template<class T> class BST
     {
         public:
-            BST();
-            ~BST();
+            BST(); // Default Constructor
+            BST(const BST & source); // Copy Constructor
+            ~BST(); // Destructor
 
-            int vtx_count() const;
+            int vtx_count() const; // The # of vertices in the BST
 
-            void insert(const T & element);
+            // Insert a new element, in order, into the BST
+            void insert(const T & element); 
+            // Remove an element from the BST, if it exists. Return true
+            // if removal is successful, or false otherwise
             bool remove(const T & element);
 
+            // Prints all elements of the BST in order
             void print() const;
+
+            // Converts the BST to a .dot file, saving to
+            // supplied filename
             void to_dot(std::string filename) const;
 
         private:
-            BSTNode<T> *root;
-            int vertex_count;
+            BSTNode<T> *root; // Root of the Tree
+            int vertex_count; // # of vertices
 
             void insert_helper(const T & element, BSTNode<T> *& current);
             bool remove_helper(const T & element, BSTNode<T> *& current);
@@ -66,29 +74,30 @@ namespace bst
     // Pointer type
     template<class T> struct BSTNode<T*>
     {
-        T* element;
-        BSTNode<T> *left;
-        BSTNode<T> *right;
+        T* element; // Pointer-value of type T* being stored in node
+        BSTNode<T> *left; // left child
+        BSTNode<T> *right; // right child
         
         BSTNode();
         BSTNode(const BSTNode<T*> & source);
         BSTNode(T *elem);
         ~BSTNode();
     };
-
-    template<class T> class BST<T*>
-    { 
-
-    };
-
+    
     /* IMPLEMENTATION OF BST
      * included in header, to avoid templatiie
      * inclusion issues */
+
     /* Non-Pointer Type */
+    /* BSTNode */
+    // Default Constructor
     template<class T> BSTNode<T>::BSTNode() : element(NULL), left(NULL), right(NULL) { } 
 
+    // Primary Constructor
     template<class T> BSTNode<T>::BSTNode(const T & elem) : left(NULL), right(NULL)
     {
+        // If the element is a primitive type, simply set element to elem.
+        // Otherwise call the element's Copy Constructor
         if(std::is_fundamental<T>())
         {
             element = elem;
@@ -99,33 +108,50 @@ namespace bst
         }
     }
 
+    // Destructor - recursively deletes each BSTNode's children
     template<class T> BSTNode<T>::~BSTNode() 
     {
         delete left;
         delete right;
     }
 
+    // Default Constructor - initializes data
     template<class T> BST<T>::BST() : root(NULL), vertex_count(0) { }
+    // Destructor - deletes root, which recursively deletes all vertices
     template<class T> BST<T>::~BST() { delete root; root = NULL; }
 
+    // Returns the # of vertices in the BST, using cached vertex_count
     template<class T> int BST<T>::vtx_count() const { return vertex_count; }
 
+    // Insert a new element, in order, into the BST
     template<class T> void BST<T>::insert(const T & element)
     {
+        // Call helper function
         insert_helper(element, root);
     }
+
+    // Remove an element from the BST, if it exists. Return true
+    // if removal is successful, or false otherwise
     template<class T> bool BST<T>::remove(const T & element)
     {
+        // Return the helper function result
         return remove_helper(element, root); 
     }
+
+    // Prints all elements of the BST in order
     template<class T> void BST<T>::print() const
     {
+        // Call the helper function, followed by a newline
         print_helper(root);    
         std::cout << std::endl;
     }
 
+    // Searches the BST for the correct position to insert new element, traversive each
+    // subnode through the process
     template<class T> void BST<T>::insert_helper(const T & element, BSTNode<T> *& current)
     {
+        // If current is NULL, correct node has been reached
+        // Set current to new BSTNode with element value
         if(current == NULL) 
         {
             current = new BSTNode<T>(element);
@@ -261,7 +287,7 @@ namespace bst
     }
     template<class T> BSTNode<T*>::BSTNode(const BSTNode<T*> & source)
     {
-        element = new T(*(source.element)); 
+        element = new typeof(source)(*(source.element)); 
         left = right = NULL;
     }
 
--- main.cpp
+++ main.cpp
@@ -11,26 +11,12 @@ int main(int argc, char **argv)
     using namespace bst;
     using namespace std;
 
-    BST<string> bst = BST<string>();
-    bst.insert("Hello");
-    bst.insert("World!");
-    bst.insert("My");
-    bst.insert("Name");
-    bst.insert("Is");
-    bst.insert("Oliver!");
-    bst.insert("And");
-    bst.insert("I");
-    bst.insert("Have");
-    bst.insert("Two");
-    bst.insert("Masters,");
-    bst.insert("Alyssa");
-    bst.insert("Chris!");
-
-    bst.print();
+    BST<Account*> accounts = BST<Account*>();
+    accounts.insert(new SavingsAccount(10.0, "Christian", 0.23));
 
     if(argc > 1 && strcmp(argv[1], "--dot") == 0)
     {
-        bst.to_dot(argv[2]); 
+        accounts.to_dot(argv[2]); 
     }
 
     return 0;
